257241b8391e9900d3abfd9809223871f85e8bf3
Guilherme Sandi <guilherme_sandi@hotmail.com>
2020-11-25 20:17:49 -0300

    Update upload.ts

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -5,7 +5,7 @@ import multer from 'multer';
 export default {
   storage: multer.diskStorage({
     destination: path.resolve(__dirname, '..', '..', 'tmp'),
-    filename: (request, file, callback) {
+    filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${fileHash}-${file.originalname}`;
 
