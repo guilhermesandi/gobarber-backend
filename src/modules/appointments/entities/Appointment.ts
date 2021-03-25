c53be4bd96b0ecb5db49de9fe03b4e2c73391df8
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-03-25 20:04:06 -0300

    Separating in modules

--- src/modules/appointments/entities/Appointment.ts
+++ src/modules/appointments/entities/Appointment.ts
@@ -1,6 +1,14 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
 
-import User from './User';
+import User from '../../users/entities/User';
 
 @Entity('appointments')
 class Appointment {
--- src/shared/middlewares/ensureAuthenticated.ts
+++ src/shared/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
-import authConfig from '../config/auth';
+import authConfig from '../../config/auth';
 
 import AppError from '../errors/AppError';
 
@@ -14,7 +14,7 @@ interface TokenPayload {
 export default function ensureAuthenticated(
   request: Request,
   response: Response,
-  next: NextFunction
+  next: NextFunction,
 ): void {
   const authHeader = request.headers.authorization;
 
