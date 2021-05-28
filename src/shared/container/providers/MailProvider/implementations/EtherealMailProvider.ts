9f4741309b0f6f5068f71eda8117181defbe6944
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-05-28 18:52:47 -0300

    Install nodemailer and create EtherealMailProvider

---
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -0,0 +1,10 @@
+import nodemailer from 'nodemailer';
+import IMailProvider from '../models/IMailProvider';
+
+export default class EtherealMailProvider implements IMailProvider {
+  constructor() {
+    const account = nodemailer.createTestAccount();
+  }
+
+  public async sendMail(to: string, body: string): Promise<void> {}
+}
