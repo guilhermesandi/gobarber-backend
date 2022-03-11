6f40a6aa5e449587a23de4f6ace9a730ae249e2c
Guilherme Sandi <guilherme_sandi@hotmail.com>
2022-03-10 21:37:36 -0300

    Avatar file upload

---
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -0,0 +1,48 @@
+import uploadConfig from '@config/upload';
+import aws, { S3 } from 'aws-sdk';
+import fs from 'fs';
+import path from 'path';
+import IStorageProvider from '../models/IStorageProvider';
+
+class DiskStorageProvider implements IStorageProvider {
+  private client: S3;
+
+  constructor() {
+    this.client = new aws.S3({
+      region: 'us-east-1',
+    });
+  }
+
+  public async saveFile(file: string): Promise<string> {
+    const originalPath = path.resolve(uploadConfig.tmpFolder, file);
+
+    const fileContent = await fs.promises.readFile(originalPath, {
+      encoding: 'utf-8',
+    });
+
+    this.client
+      .putObject({
+        Bucket: 'app-gobarber-sandi',
+        Key: file,
+        ACL: 'public-read',
+        Body: fileContent,
+        ContentType: 'image/jpeg',
+      })
+      .promise();
+
+    return file;
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    const filePath = path.resolve(uploadConfig.uploadsFolder, file);
+
+    try {
+      await fs.promises.stat(filePath);
+    } catch {
+      return;
+    }
+    await fs.promises.unlink(filePath);
+  }
+}
+
+export default DiskStorageProvider;
